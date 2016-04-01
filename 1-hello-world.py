05ab2401bf8490983692e689074f0a6edc0ad5da
Brandon DeRosier <x@bdero.me>
2016-03-31 22:54:59 -0400

    Initial commit

---
+++ 1-hello-world.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import sys
+
+import OpenGL.GL as gl
+import OpenGL.GLUT as glut
+
+
+def display():
+    glut.glutSwapBuffers()
+
+
+def reshape(width, height):
+    gl.glViewport(0, 0, width, height)
+
+
+def keyboard(key, x, y):
+    print(key, x, y)
+    if key in [b'\x03', b'\x1b']:
+        sys.exit()
+
+
+if __name__ == '__main__':
+    glut.glutInit()
+    glut.glutInitDisplayMode(glut.GLUT_DOUBLE | glut.GLUT_RGBA)
+    glut.glutCreateWindow("Hello world")
+    glut.glutReshapeWindow(512, 512)
+
+    glut.glutReshapeFunc(reshape)
+    glut.glutDisplayFunc(display)
+    glut.glutKeyboardFunc(keyboard)
+
+    glut.glutMainLoop()
