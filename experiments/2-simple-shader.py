fdbdfb627020fa9ba169982e4a8b55a711395ef8
Brandon DeRosier <x@bdero.me>
2016-04-01 00:51:27 -0400

    Add shader attribute mapping for 2

--- experiments/2-simple-shader.py
+++ experiments/2-simple-shader.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+import ctypes
+import numpy
+import OpenGL.GL as gl
+
 import utils
 
 
@@ -7,9 +11,12 @@ VERT = """
 uniform float scale;
 attribute vec2 position;
 attribute vec4 color;
+varying vec4 v_color;
+
 void main()
 {
     gl_Position = vec4(position*scale, 0.0, 1.0);
+    v_color = color;
 }
 """
 
@@ -23,6 +30,32 @@ void main()
 """
 
 
+def load_shader(vertex, fragment):
+    program = utils.build_program(vertex, fragment)
+
+    data = numpy.zeros(4, dtype=[("position", numpy.float32, 3),
+                                 ("color", numpy.float32, 4)])
+
+    buffer = gl.glGenBuffers(1)
+
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, buffer)
+    gl.glBufferData(gl.GL_ARRAY_BUFFER, data.nbytes, data, gl.GL_DYNAMIC_DRAW)
+
+    stride = data.strides[0]
+
+    offset = ctypes.c_void_p(0)
+    loc = gl.glGetAttribLocation(program, "position")
+    gl.glEnableVertexAttribArray(loc)
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, buffer)
+    gl.glVertexAttribPointer(loc, 3, gl.GL_FLOAT, False, stride, offset)
+
+    offset = ctypes.c_void_p(data.dtype["position"].itemsize)
+    loc = gl.glGetAttribLocation(program, "color")
+    gl.glEnableVertexAttribArray(loc)
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, buffer)
+    gl.glVertexAttribPointer(loc, 4, gl.GL_FLOAT, False, stride, offset)
+
+
 if __name__ == '__main__':
-    utils.initWindow()
-    utils.build_program(VERT, FRAG)
+    with utils.initWindow():
+        load_shader(VERT, FRAG)
