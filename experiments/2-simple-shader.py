765d7afb662b1b1273cb82a7d454f3cdb99f38c8
Brandon DeRosier <x@bdero.me>
2016-04-01 02:01:40 -0400

    Added animated scaling to 2-simple-shader

--- experiments/2-simple-shader.py
+++ experiments/2-simple-shader.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import numpy
 import OpenGL.GL as gl
 from vispy.gloo import Program
 
@@ -39,3 +40,7 @@ if __name__ == '__main__':
         def render():
             program.draw(gl.GL_TRIANGLE_STRIP)
         utils.display_callbacks.append(render)
+
+        def update(dt):
+            program['scale'] = (1.5 + numpy.cos(utils.clock))/4.0
+        utils.update_callbacks.append(update)
